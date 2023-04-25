1b7ba0f00461732aa704204c3d58b9d99e9195db
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-04-25 20:39:59 +0700

    Accomplish UI WelcomeScreen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,52 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, Image, StyleSheet, TouchableOpacity, Text } from "react-native";
 
 const WelcomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Image source={require("../assets/little-lemon-logo.png")} />
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/little-lemon-logo.png")}
+        style={styles.image}
+      />
+
+      <View>
+        <Text style={{ fontSize: 22, textAlign: "center" }}>
+          Little Lemon, your local Mediterranean Bistro
+        </Text>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Subcribe")}
+      >
+        <Text style={{ fontSize: 16, color: "#fff", textAlign: "center" }}>
+          Newsletter
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 25,
+    flex: 0.8,
+  },
+  image: {
+    width: 165,
+    height: 165,
+    resizeMode: "contain",
+    marginBottom: 30,
+  },
+  button: {
+    position: "absolute",
+    width: 300,
+    padding: 10,
+    backgroundColor: "#40524c",
+    borderRadius: 5,
+    bottom: -100,
+  },
+});
